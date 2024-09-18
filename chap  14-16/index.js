fdfadaa299b896c43bbb1e114ec33e5987b43d67
aaliyansuhail <mrsuhail76@gmail.com>
2024-09-18 23:21:51 +0500

    publish branch

---
+++ chap  14-16/index.js
@@ -0,0 +1,27 @@
+// Question :  1
+
+
+// var user=prompt('Enter your age');
+// var name="My age is 14 years";
+// if (user == age) {
+//     document.write(age)
+// } else {
+//     document.write("Enter write your age")
+// }
+
+//Question :  2
+
+// var cars=[""];
+
+// Question :  3
+
+// var hello = [""];
+
+//Question :  4
+
+// var name=["Aaliyan"];
+
+// Question :  5
+
+// var my-qualification=["SSC <br>", "HSC <br>", "BSC <br>","BS <br>", "BCOM <br>", "MS <br>" , "M.Phil. <br>" , "PHD <br>"];
+// document.write(my-qualification)"
---
+++ chap.12-13/index.js
@@ -0,0 +1,145 @@
+//  question : 1
+
+
+
+
+
+// var input = prompt("Enter a Value"); 
+
+// if (input === "") {
+//     document.write("Enter a Value.");
+// } else {
+//     var char = input[0];
+
+//     if (char >= '0' && char <= '10') {
+//         document.write(char + " is a number.");
+//     } else if (char >= "A" && char <= "Z") {
+//         document.write(char + " is uppercase letter.");
+//     } else if (char >= 'a' && char <= 'z') {
+//         document.write(char + " is lowercase letter.");
+//     } else {
+//         document.write(char + " is not a number and not a letter.");
+//     }
+// }
+
+//  question : 2
+
+
+
+
+
+
+
+// var hello = (prompt("Enter the first value:"));
+// var bye = (prompt("Enter the second value:"));
+
+// if (hello === bye) {
+//     document.write("Both values are equal.");
+// } else {
+//     var larger = (hello > bye) ? hello : bye;
+//     document.write("The larger value is: " + larger);
+// }
+
+
+//  question : 3
+
+
+
+
+
+
+
+
+// var number = parseFloat(prompt("Enter a value:")); 
+
+// if (number > 0) {
+//     document.write("The number is positive.");
+// } else if (number < 0) {
+//     document.write("The number is negative.");
+// } else {
+//     document.write("The number is zero.");
+// }
+
+//  question : 4
+
+
+
+
+
+
+// var user=prompt("Enter an alphabet");
+// var alphabet1="a";
+// var alphabet2='b';
+// var alphabet3='c';
+// var alphabet4='d';
+// var alphabet5='e'
+// if (user == alphabet1 || user == alphabet2 || user == alphabet3 || user == alphabet4 || user == alphabet5) {
+//     alert("these are starting 5 alpahbets")
+// } else {
+//     alert("false");
+// }
+
+//  question : 5
+
+
+
+
+
+
+
+
+// var password="8*)*#*0889808";
+// alert("Enter your password?")
+
+// var user=prompt("Enter the password?");
+// if (user == password) {
+//     alert("Correct!")
+// } else {
+//     alert("This is wrong password")
+// }
+
+//  question : 6
+
+
+
+
+
+
+
+// var greeting1 = "good morning";
+// var greeting2 = "good evening";
+// var user=prompt("Enter time")
+// if (user <= 12) {
+//     alert("Good morning")
+// } else if (user >= 12) {
+//     alert("Good evening")
+    
+// }
+
+//  question : 7
+
+
+
+
+
+
+
+
+
+// var time1=2900;
+// var time2=3000;
+// var time3=4100;
+// var time4=5200;
+// var time5=6300;
+// var user=prompt("Enter NO and check time? 2900 , 3000 , 4100 , 5200 , 6300")
+// if (user == time1  ) {
+//     alert("7pm")
+// } else if (user == time1) {
+//    alert("8pm"); 
+// }else if(user == time2){
+//     alert("9pm")
+// }else if(user == time3){
+//     alert("10pm")
+// }else if (user == time4){
+//     alert("11pm")
+// }
